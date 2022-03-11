a7b9ea02fd55d833954b409245d971b09d6440ca
jack mcdonnell <91698558+jackmcdonnell713@users.noreply.github.com>
2022-03-11 16:52:14 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,25 @@
+#dependencies
 library(dplyr)
-library(readr)
+
+#Read in MPG csv
+
 MechaCar_mpg <- read_csv("MechaCar_mpg.csv")
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar) #generate MLM regression model
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar)) #summary
+
+#Create MLM regression model
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar) 
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar)) 
+
+#Read in suspension coil csv
+
+Suspension_Coil <- read_csv("Suspension_Coil.csv")
+
+
+#Total summary table creation
+total_summary <- Suspension_Coil %>% summarise(mean(PSI), median(PSI), var(PSI), sd(PSI), .groups = 'keep')
+                                             
+total_summary
+
+#Lot Summary table creation
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI),median(PSI),var(PSI), sd(PSI), .groups = 'keep')
+
+lot_summary
