cc49c8ecc924f630fa27c69cf523d5deacdc314a
jack mcdonnell <91698558+jackmcdonnell713@users.noreply.github.com>
2022-03-11 17:21:25 -0600

    Add files via upload
    Deliverable 3 code included

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -20,6 +20,18 @@ total_summary <- Suspension_Coil %>% summarise(mean(PSI), median(PSI), var(PSI),
 total_summary
 
 #Lot Summary table creation
-lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI),median(PSI),var(PSI), sd(PSI), .groups = 'keep')
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarise(mean(PSI),median(PSI),var(PSI), sd(PSI), .groups = 'keep')
 
 lot_summary
+
+
+# First T-test for PSI difference
+t.test(Suspension_Coil$PSI, mu = 1500)
+
+#Help determining arguments from challenge page
+?t.test()
+
+#Using subset to determine if each lot is different from the mean
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(Suspension_Coil ,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(Suspension_Coil ,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
